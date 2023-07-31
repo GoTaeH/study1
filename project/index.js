5ffee7f00b448a60d3e8396330ab7dfe64a802b0
gotaeh <ktt2341@naver.com>
2023-08-01 05:26:51 +0900

    project_2

--- project/index.js
+++ project/index.js
@@ -6,19 +6,24 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname));
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/main/main.html');
 });
 app.get('/header', (req, res) => {
     res.sendFile(__dirname + '/header.html');
 });
+
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/login/login.html');
 });
+app.get('/login/membership', (req, res) => {
+    res.sendFile(__dirname + '/login/membership.html');
+});
+
 app.get('/bookmark', (req, res) => {
     res.sendFile(__dirname + '/bookmark/bookmark.html');
 });
+
 app.get('/style/:name', (req, res) => {
     res.sendFile(__dirname + '/style/' + req.params.name);
 });
